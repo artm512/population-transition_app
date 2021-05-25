90677112900b176add5f82cf12f3eda224e02dd8
artm512 <ArataMakino@makinoshin-no-MacBook-Pro.local>
2021-05-25 23:36:16 +0900

    feat: マークアップ チェックボックス・ラベル

--- pages/index.vue
+++ pages/index.vue
@@ -1,63 +1,116 @@
 <template>
-  <div class="container">
-    <div>
-      <Logo />
-      <h1 class="title">population-transition_app</h1>
-      <div class="links">
-        <a
-          href="https://nuxtjs.org/"
-          target="_blank"
-          rel="noopener noreferrer"
-          class="button--green"
+  <div :class="$style.container">
+    <h1 :class="$style.title">都道府県毎の人口推移</h1>
+    <ul :class="$style.checkList">
+      <li
+        v-for="data in dataSetPref"
+        :key="data.prefCode"
+        :class="$style.checkList__item"
+      >
+        <label
+          :class="$style.checkList__checkbox"
+          :for="`pref__checkbox-${data.prefCode}`"
         >
-          Documentation
-        </a>
-        <a
-          href="https://github.com/nuxt/nuxt.js"
-          target="_blank"
-          rel="noopener noreferrer"
-          class="button--grey"
-        >
-          GitHub
-        </a>
-      </div>
-    </div>
+          <input :id="`pref__checkbox-${data.prefCode}`" type="checkbox" />{{
+            data.prefName
+          }}
+        </label>
+      </li>
+    </ul>
+    <div :class="$style.graph">graph</div>
   </div>
 </template>
 
 <script>
-export default {}
+// dummyData
+const DUMMY_PREF = [
+  { prefCode: 1, prefName: '北海道' },
+  { prefCode: 2, prefName: '青森県' },
+  { prefCode: 3, prefName: '岩手県' },
+  { prefCode: 4, prefName: '宮城県' },
+  { prefCode: 5, prefName: '秋田県' },
+  { prefCode: 6, prefName: '山形県' },
+  { prefCode: 7, prefName: '福島県' },
+  { prefCode: 8, prefName: '茨城県' },
+  { prefCode: 9, prefName: '栃木県' },
+  { prefCode: 10, prefName: '群馬県' },
+  { prefCode: 11, prefName: '埼玉県' },
+  { prefCode: 12, prefName: '千葉県' },
+  { prefCode: 13, prefName: '東京都' },
+  { prefCode: 14, prefName: '神奈川県' },
+  { prefCode: 15, prefName: '新潟県' },
+  { prefCode: 16, prefName: '富山県' },
+  { prefCode: 17, prefName: '石川県' },
+  { prefCode: 18, prefName: '福井県' },
+  { prefCode: 19, prefName: '山梨県' },
+  { prefCode: 20, prefName: '長野県' },
+  { prefCode: 21, prefName: '岐阜県' },
+  { prefCode: 22, prefName: '静岡県' },
+  { prefCode: 23, prefName: '愛知県' },
+  { prefCode: 24, prefName: '三重県' },
+  { prefCode: 25, prefName: '滋賀県' },
+  { prefCode: 26, prefName: '京都府' },
+  { prefCode: 27, prefName: '大阪府' },
+  { prefCode: 28, prefName: '兵庫県' },
+  { prefCode: 29, prefName: '奈良県' },
+  { prefCode: 30, prefName: '和歌山県' },
+  { prefCode: 31, prefName: '鳥取県' },
+  { prefCode: 32, prefName: '島根県' },
+  { prefCode: 33, prefName: '岡山県' },
+  { prefCode: 34, prefName: '広島県' },
+  { prefCode: 35, prefName: '山口県' },
+  { prefCode: 36, prefName: '徳島県' },
+  { prefCode: 37, prefName: '香川県' },
+  { prefCode: 38, prefName: '愛媛県' },
+  { prefCode: 39, prefName: '高知県' },
+  { prefCode: 40, prefName: '福岡県' },
+  { prefCode: 41, prefName: '佐賀県' },
+  { prefCode: 42, prefName: '長崎県' },
+  { prefCode: 43, prefName: '熊本県' },
+  { prefCode: 44, prefName: '大分県' },
+  { prefCode: 45, prefName: '宮崎県' },
+  { prefCode: 46, prefName: '鹿児島県' },
+  { prefCode: 47, prefName: '沖縄県' },
+]
+
+export default {
+  data() {
+    return {
+      dataSetPref: [],
+    }
+  },
+  mounted() {
+    this.dataSetPref = DUMMY_PREF
+  },
+}
 </script>
 
-<style>
+<style module lang="scss">
 .container {
+  max-width: 980px;
   margin: 0 auto;
-  min-height: 100vh;
-  display: flex;
-  justify-content: center;
-  align-items: center;
   text-align: center;
 }
 
-.title {
-  font-family: 'Quicksand', 'Source Sans Pro', -apple-system, BlinkMacSystemFont,
-    'Segoe UI', Roboto, 'Helvetica Neue', Arial, sans-serif;
-  display: block;
-  font-weight: 300;
-  font-size: 100px;
-  color: #35495e;
-  letter-spacing: 1px;
-}
+.checkList {
+  display: flex;
+  flex-wrap: wrap;
+  padding-left: 0;
+  list-style-type: none;
 
-.subtitle {
-  font-weight: 300;
-  font-size: 42px;
-  color: #526488;
-  word-spacing: 5px;
-  padding-bottom: 15px;
-}
+  // .checkList__item
+  &__item {
+    padding: 10px;
+  }
 
-.links {
-  padding-top: 15px;
+  // .checkList__checkbox
+  &__checkbox {
+    cursor: pointer;
+
+    > input[type='checkbox'] {
+      cursor: pointer;
+      margin-right: 5px;
+    }
+  }
 }
 </style>
