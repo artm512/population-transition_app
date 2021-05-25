d849d111d9868b9a710d3c6b416b02f2dc2b8b14
artm512 <ArataMakino@makinoshin-no-MacBook-Pro.local>
2021-05-26 01:43:13 +0900

    fix: default dataSetPopulation data

--- pages/index.vue
+++ pages/index.vue
@@ -82,7 +82,7 @@ export default {
   data() {
     return {
       dataSetPref: [],
-      dataSetPopulation: [],
+      dataSetPopulation: {},
       chartOption: {
         responsive: true,
         scales: {
