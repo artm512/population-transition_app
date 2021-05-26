8d81f2318dfbd748d880ad243ea2461c50fd4dbc
artm512 <ArataMakino@makinoshin-no-MacBook-Pro.local>
2021-05-26 23:22:57 +0900

    feat: webフォント導入（Noto Sans JP）

--- nuxt.config.js
+++ nuxt.config.js
@@ -39,7 +39,15 @@ export default {
   ],
 
   // Modules: https://go.nuxtjs.dev/config-modules
-  modules: [],
+  modules: ['nuxt-webfontloader'],
+
+  // other
+  webfontloader: {
+    google: {
+      // https://fonts.googleapis.com/css2?family=Noto+Sans+JP&display=swap
+      families: ['Noto Sans JP'],
+    },
+  },
 
   // Build Configuration: https://go.nuxtjs.dev/config-build
   build: {},
